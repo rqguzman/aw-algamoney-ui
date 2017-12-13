b0dcb06807d9f73f0b0ffe949722ba71d4a45307
rqguzman <rafael.guzman@outlook.com>
2017-12-13 06:36:20 -0200

    Improved error handling messages

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,3 +1,4 @@
+import { Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import { ToastyService } from 'ng2-toasty';
@@ -8,10 +9,25 @@ export class ErrorHandlerService {
   constructor(private toastyService: ToastyService) { }
 
   handle(errorResponse: any) {
+
     let msg: string;
 
     if (typeof errorResponse === 'string') {
+
       msg = errorResponse;
+
+    } else if (errorResponse instanceof Response
+                      && errorResponse.status >= 400
+                      && errorResponse.status <= 499) {
+
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+      try {
+        msg = errorResponse.json()[0].mensagemUsuario;
+      } catch (e) {
+        console.error('Ocorreu um erro.', errorResponse);
+      }
+
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
       console.log('Ocorreu um erro', errorResponse);
