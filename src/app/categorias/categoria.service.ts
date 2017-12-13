141052b4a661c97b50bf5054575a46df98563d37
rqguzman <rafael.guzman@outlook.com>
2017-12-13 16:29:36 -0200

    Implemented a Categoria service

---
+++ src/app/categorias/categoria.service.ts
@@ -0,0 +1,20 @@
+import { Http, Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CategoriaService {
+
+  categoriasUrl: 'http://localhost:8080/categorias';
+
+  constructor(private http: Http) { }
+
+  listarTodas(): Promise<any> {
+    const cabecalhos = new Headers();
+    cabecalhos.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.get(`${this.categoriasUrl}`, { headers: cabecalhos })
+      .toPromise()
+      .then(response => response.json());
+  }
+
+}
